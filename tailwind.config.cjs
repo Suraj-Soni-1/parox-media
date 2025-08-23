7777dc60f0b9cc4ad63e7e3fd591b97826b75b7b
Suraj-Soni-1 <surajsoni1092@gmail.com>
2025-08-23 12:59:24 +0530

    tailwind error solved

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,16 +5,3 @@ module.exports = {
   },
   plugins: [],
 }
-
-@keyframes scroll {
-  0% {
-    transform: translateX(0);
-  }
-  100% {
-    transform: translateX(-50%);
-  }
-}
-
-.animate-scroll {
-  animation: scroll 20s linear infinite;
-}
